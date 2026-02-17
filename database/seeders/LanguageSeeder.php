dd184b082df7a6435a7b37d417220aa203f8c7e7
salem <salembashndoof@gmail.com>
2026-02-17 20:38:20 +0300

    feat: Add LanguageSeeder for initial language data.

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -10,18 +10,23 @@ class LanguageSeeder extends Seeder
     public function run(): void
     {
         $languages = [
-            ['LanguageName' => 'العربية'],
-            ['LanguageName' => 'الإنجليزية'],
-            ['LanguageName' => 'الفرنسية'],
-            ['LanguageName' => 'الألمانية'],
-            ['LanguageName' => 'الإسبانية'],
-            ['LanguageName' => 'التركية'],
-            ['LanguageName' => 'الصينية'],
-            ['LanguageName' => 'اليابانية'],
+            ['LanguageID' => 1, 'LanguageName' => 'العربية'], // Arabic
+            ['LanguageID' => 2, 'LanguageName' => 'الإنجليزية'], // English
+            ['LanguageID' => 3, 'LanguageName' => 'الفرنسية'], // French
+            ['LanguageID' => 4, 'LanguageName' => 'الألمانية'], // German
+            ['LanguageID' => 5, 'LanguageName' => 'الإسبانية'], // Spanish
+            ['LanguageID' => 6, 'LanguageName' => 'التركية'], // Turkish
+            ['LanguageID' => 7, 'LanguageName' => 'الصينية'], // Chinese
+            ['LanguageID' => 8, 'LanguageName' => 'اليابانية'], // Japanese
         ];
 
+        Language::unguard();
         foreach ($languages as $language) {
-            Language::firstOrCreate(['LanguageName' => $language['LanguageName']], $language);
+            Language::firstOrCreate(
+                ['LanguageName' => $language['LanguageName']],
+                ['LanguageID' => $language['LanguageID']]
+            );
         }
+        Language::reguard();
     }
 }
